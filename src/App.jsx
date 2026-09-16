44b56a84a0ddda6406fd34db68ae175b1c732682
JoycePamO <joy94ortega@gmail.com>
2026-09-16 15:43:04 +0100

    Initial commit - React Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-primary">Dictionary App</h1>
+      <button className="btn btn-primary mt-3">Test Bootstrap</button>
+    </div>
+  );
+}
+
+export default App;
