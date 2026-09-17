5c4e64ffff7c130cf1c03acc2490ca1db8e39dbc
JoycePamO <joy94ortega@gmail.com>
2026-09-17 20:53:13 +0100

    reducing the visual section from 6 to 4 images

--- src/services/pexelsApi.js
+++ src/services/pexelsApi.js
@@ -2,7 +2,7 @@ const API_URL = "https://api.pexels.com/v1/search";
 
 export async function searchImages(query) {
   const response = await fetch(
-    `${API_URL}?query=${encodeURIComponent(query)}&per_page=6`,
+    `${API_URL}?query=${encodeURIComponent(query)}&per_page=4`,
     {
       headers: {
         Authorization: import.meta.env.VITE_PEXELS_API_KEY,
